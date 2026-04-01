8b7d141729bed28705852e5f663da10c3ddd0fcb
SUNIL <sunilmallela9177@gmail.com>
2026-04-01 22:00:04 +0530

    Method Security

--- src/main/java/com/example/SpringBoot/controller/HelloController.java
+++ src/main/java/com/example/SpringBoot/controller/HelloController.java
@@ -1,14 +1,28 @@
 package com.example.SpringBoot.controller;
 
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class HelloController {
-    @GetMapping("/hello")
-    public String hello(){
-        return "Welcome to SpringSecurity";
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    @GetMapping("/user")
+     public String user(){
+         return "Welcome User";
+     }
+     @PreAuthorize("hasRole('ADMIN')")
+     @GetMapping("/admin")
+     public String admin(){
+        return "Welcome Admin";
+     }
+    @PostAuthorize("returnObject == authentication.name")
+    @GetMapping("/profile/{username}")
+    public String getProfile(@PathVariable String username) {
+        return username;
     }
 }
--- src/main/java/com/example/SpringBoot/security/SecurityFilter.java
+++ src/main/java/com/example/SpringBoot/security/SecurityFilter.java
@@ -3,11 +3,13 @@ package com.example.SpringBoot.security;
 import com.example.SpringBoot.filter.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityFilter {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     public SecurityFilter(JwtAuthenticationFilter jwtAuthenticationFilter) {
@@ -20,6 +22,8 @@ public class SecurityFilter {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        //.requestMatchers("/api/user").hasAnyRole("USER","ADMIN")
+                        //.requestMatchers("/api/admin").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
